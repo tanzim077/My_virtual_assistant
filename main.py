625bea65cb0b477f5294bcf7d8987eb81457eefc
Tanzim Ahmed <39195600+tanzim077@users.noreply.github.com>
2021-03-22 21:12:17 +0600

    Some functionality added

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import warnings
 import calendar
 import random
 import wikipedia
+import os
 
 warnings.filterwarnings('ignore')
 
@@ -28,8 +29,8 @@ def recordAudio ():
 
         data = ''
         try:
-            data = r.recognize_google(audio)      # This is for default English language
-            # data = r.recognize_google(audio, language= "bn-BD") # This is for Bengali language
+            # data = r.recognize_google(audio)      # This is for default English language
+            data = r.recognize_google(audio, language= "bn-BD") # This is for Bengali language
             print('You said ' + '"' + data + '"')
         except sr.UnknownValueError:
             print('There is an unknown error')
@@ -38,5 +39,56 @@ def recordAudio ():
 
         return data
 
-recordAudio()
+# recordAudio()
 
+def assistantrRespnse(text):
+    print(text)
+    myobj = gTTS(text=text, lang='en', slow=False)
+    # myobj = gTTS(text=text, lang='fr', slow=False) # Unfortunately Bengali not supported in gTTS 'lang'
+    myobj.save('Assistant_response.mp3')
+    os.system('start Assistant_response.mp3')
+
+# text = 'This is a test'
+# text = "আমার সোনার বাংলা "
+# assistantrRespnse(text)
+
+def wakeWords(text):
+    WAKE_WORDS = ['hey pc', 'hey system']
+    text = text.lower()
+    for item in WAKE_WORDS:
+        if item in text:
+            return True
+    return False
+
+def getDate():
+    now = datetime.datetime.now()
+    today = datetime.datetime.today()
+    weekday = calendar.day_name[today.weekday()]
+    monthNum = now.month
+    dayNum = now.day
+
+    months_name = ['January', 'February','March', 'April','May','June', 'July','August',
+                   'September','October','November','December']
+    ordinal_numbers = ['1st','2nd','3rd','4th','5th','6th','7th','8th','9th','10th',
+                       '11th','12th','13th','14th','15th','16th','17th','18th','19th','20th',
+                       '21st','22nd','23rd','24th','25th','26th','27th','28th','29th','30th' '31st']
+    return 'Today is ' + weekday + ' ' + months_name[monthNum - 1] + ' ' + 'the' + ' ' + ordinal_numbers[dayNum-1] + '.'
+
+# print(getDate())
+
+def gretting(text):
+    GREETING_INPUTS = ['Hey', 'Hello', 'Hola', 'Whats Up', 'Hey Welcome']
+    GREETING_RESPONSE = ['howdy', 'What\'s good', 'hello too', 'thanks']
+
+    for word in text.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSE) +'.'
+
+        return '...'
+# Need to customize more
+def getPerson(text):
+    wordlist = text.split()
+
+    for i in range(0, len(wordlist)):
+        if i + 3 <= len(wordlist) - 1 and wordlist[i].lower() == 'who' and wordlist[i + 1].lower() == 'is':
+            return wordlist[i +2] + ' ' + wordlist[i+3]
